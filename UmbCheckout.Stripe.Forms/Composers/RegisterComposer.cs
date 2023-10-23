489000a87e432fc375a22bdfa3f1b6b9be2af2c5
Aaron Sadler <aaron@umbhost.net>
2023-10-23 11:15:45 +0100

    Added Workflow

---
+++ UmbCheckout.Stripe.Forms/Composers/RegisterComposer.cs
@@ -0,0 +1,15 @@
+﻿using UmbCheckout.Stripe.Forms.Workflows;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Forms.Core.Providers.Extensions;
+
+namespace UmbCheckout.Stripe.Forms.Composers
+{
+    public class RegisterComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.FormsWorkflows().Add<SendToUmbCheckoutWorkflow>();
+        }
+    }
+}
